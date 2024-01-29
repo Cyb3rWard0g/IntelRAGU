f5b41fea9cb7457322318bde37a2d1470617de7a
Roberto Rodriguez <rrodri0622@gmail.com>
2024-01-29 10:51:14 -0500

    Added images and descriptions

--- README.md
+++ README.md
@@ -19,3 +19,15 @@ OPENAI_API_KEY=XXXXXXXXX
 ```
 docker run -it --rm --name rag-chroma --env-file .env -p 8080:8080 rag-chroma
 ```
+
+After running that command, the container will:
+* Download the [all-mpnet-base-v2 sentence-transformers model](https://huggingface.co/sentence-transformers/all-mpnet-base-v2) (~400MB)
+* Download the [Hugging Face Cyb3rWard0g/ATTCKGroups dataset](https://huggingface.co/datasets/Cyb3rWard0g/ATTCKGroups) (~846KB)
+* Process the dataset by tokenizing and embedding every ATT&CK Group
+* Creat the vector database by adding all embedding into it.
+
+## Explore Playground
+
+Browse to `http://127.0.0.1/rag-chroma/playground` and start asking questions.
+
+![](images/LangServer-Playground.png)
--- api/packages/rag-chroma/rag_chroma/chain.py
+++ api/packages/rag-chroma/rag_chroma/chain.py
@@ -26,37 +26,6 @@ vectorstore = Chroma.from_documents(documents=all_splits,
 retriever = vectorstore.as_retriever()
 """
 
-"""
-# Define a few variables
-import os, glob
-
-current_directory = os.path.dirname("__file__")
-documents_directory = os.path.join(current_directory, "documents")
-group_files = glob.glob(os.path.join(documents_directory, "*.md"))
-
-# Load Documents
-from langchain_community.document_loaders import UnstructuredMarkdownLoader
-
-md_docs = []
-for group in group_files:
-    print(f' [*] Loading {os.path.basename(group)}')
-    loader = UnstructuredMarkdownLoader(group)
-    md_docs.extend(loader.load())
-
-print(md_docs[0].page_content)
-
-# Split Documents
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=50,  # number of tokens overlap between chunks
-    separators=['\n\n', '\n', ' ', '']
-)
-chunks = text_splitter.split_documents(md_docs)
-
-"""
-
 # Download Dataset
 from datasets import load_dataset
 from langchain.docstore.document import Document
